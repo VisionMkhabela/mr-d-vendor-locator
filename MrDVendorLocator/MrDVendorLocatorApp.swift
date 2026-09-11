af37e06a933fe2eee4d2e8a925270e4984c1c909
Vision Mkhabela <Vision.Mkhabela@johnlewis.co.uk>
2026-09-12 00:45:49 +0200

    Initial Commit

---
+++ MrDVendorLocator/MrDVendorLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrDVendorLocatorApp.swift
+//  MrDVendorLocator
+//
+//  Created by Vision Mkhabela on 2026/09/12.
+//
+
+import SwiftUI
+
+@main
+struct MrDVendorLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
